5a09b442143301216d0ae2d764e32f1222f12d7c
stellaliang21 <stellaliang2014@gmail.com>
2019-03-13 17:09:19 -0700

    Implement schema and update tests

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,35 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+  /* Pseudocode: Create Table
+  Column 1 : ID, Integer, Auto-increment, Primary Key
+  Column 2 : Messages Text NOT NULL,
+  Column 3 : Room_ID, Integer
+  Column 4 : User_UD, Integer
+  */
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  message TEXT NOT NULL,
+  room_id INT NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  user TEXT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  room TEXT NOT NULL,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
